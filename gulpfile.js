53c33fc2e829207ac0d69a60c1fcb1917d1f26b3
Brian Harrington <bharrington@thirdandgrove.com>
2019-03-15 13:05:59 -0600

    Reorganize directory structure for src and dist. Update Gulp file to make more configurable.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,56 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    eslint       = require('gulp-eslint'),
-    scsslint     = require('gulp-sass-lint'),
+var babel        = require('gulp-babel'),
+    beeper       = require('beeper'),
+    browserSync  = require('browser-sync'),
     cache        = require('gulp-cached'),
-    prefix       = require('autoprefixer'),
-    notify       = require('gulp-notify'),
-    postcss      = require('gulp-postcss'),
-    imagemin     = require('gulp-imagemin'),
+    concat       = require('gulp-concat'),
+    cssnano      = require('gulp-cssnano'),
+    eslint       = require('gulp-eslint'),
+    gulp         = require('gulp'),
     iconfont     = require('gulp-iconfont'),
     iconfontCSS  = require('gulp-iconfont-css'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    cssnano      = require('gulp-cssnano'),
+    imagemin     = require('gulp-imagemin'),
+    notify       = require('gulp-notify'),
     plumber      = require('gulp-plumber'),
+    postcss      = require('gulp-postcss'),
+    prefix       = require('autoprefixer'),
+    sass         = require('gulp-sass'),
     sassGlob     = require('gulp-sass-glob'),
-    babel        = require('gulp-babel'),
-    browserSync  = require('browser-sync'),
+    scsslint     = require('gulp-sass-lint'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    terser       = require('gulp-terser'),
     reload       = browserSync.reload,
-    beeper       = require('beeper'),
     runTimestamp = Math.round(Date.now() / 1000);
 
 // Prefix with project code
 var fontName = 'icons';
 
 // Paths
+var localDev = 'http://local.yourlocal.com';
+
 var paths = {
-  styles: [
-    'scss/**/*.scss'
-  ],
-  scripts: [
-    'js/*.js'
-  ],
+  styles: {
+    src: 'src/scss/**/*.scss',
+    dist: 'dist/css'
+  },
+  scripts: {
+    src: 'src/js/custom/**/*.js',
+    dist: 'dist/js'
+  },
   images: {
-    src: './images/**/*',
-    svg: './images/svg/*.svg'
+    src: 'src/images/**/*',
+    dist: 'dist/images',
+    icons: 'dist/images/icons/*.svg'
+  },
+  icons: {
+    path: 'src/scss/templates/_icons-template.scss',
+    target: '../../../src/scss/global/__icons.scss',
+    fontPath: '../dist/fonts/icons/',
+    dist: 'dist/fonts/icons/'
   }
 }
 
-
 gulp.task('scss', () => {
-  return gulp.src('scss/styles.scss')
+  return gulp.src('./src/scss/styles.scss')
     .pipe(plumber({ errorHandler: function(err) {
       notify.onError({
         title: "Gulp error in " + err.plugin,
@@ -55,13 +67,13 @@ gulp.task('scss', () => {
         browsers: ['last 3 versions'],
         cascade: false })
       ]))
-    .pipe(sourcemaps.write('../css/maps'))
-    .pipe(gulp.dest('css'))
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(paths.styles.dist))
     .pipe(reload({stream:true}));
 });
 
 gulp.task("scsslint", () => {
-  return gulp.src(paths.styles)
+  return gulp.src(paths.styles.src)
     .pipe(
       scsslint({
         options: {
@@ -74,17 +86,18 @@ gulp.task("scsslint", () => {
 });
 
 gulp.task('optimize-images', () => {
-  return gulp.src(paths.images.src, {base: '.'})
-    .pipe(imagemin());
+  return gulp.src(paths.images.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.images.dist));
 });
 
 gulp.task('iconfont', () => {
-  return gulp.src(paths.images.svg)
+  return gulp.src(paths.images.icons)
     .pipe(iconfontCSS({
       fontName: fontName,
-      path: './scss/templates/_icons-template.scss',
-      targetPath: '../scss/global/__icons.scss',
-      fontPath: '../fonts/icons/',
+      path: paths.icons.path,
+      targetPath: paths.icons.target,
+      fontPath: paths.icons.fontPath,
       cacheBuster: runTimestamp
     }))
     .pipe(iconfont({
@@ -96,11 +109,11 @@ gulp.task('iconfont', () => {
       prependUnicode: true,
       timestamp: runTimestamp,
     }))
-    .pipe(gulp.dest('./fonts/icons/'));
+    .pipe(gulp.dest(paths.icons.dist));
 });
 
 gulp.task('eslint', () => {
-  return gulp.src(paths.scripts)
+  return gulp.src(paths.scripts.src)
     .pipe(eslint({
       parser: 'babel-eslint',
       rules: {
@@ -118,7 +131,7 @@ gulp.task('eslint', () => {
 });
 
 gulp.task('scripts', () => {
-  return gulp.src(paths.scripts)
+  return gulp.src(paths.scripts.src)
       .pipe(plumber({ errorHandler: function(err) {
         notify.onError({
           title: "Gulp error in " + err.plugin,
@@ -131,16 +144,19 @@ gulp.task('scripts', () => {
       }))
       .pipe(plumber())
       .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(terser())
       .pipe(sourcemaps.write('./maps'))
-      .pipe(gulp.dest('js/dist'))
+      .pipe(gulp.dest(paths.scripts.dist))
       .pipe(reload({stream:true}));
 });
 
 // Browser Sync
 gulp.task('browser-sync', () => {
   browserSync({
+    notify: false,
     proxy: {
-      target: "http://local.yourlocal.com"
+      target: localDev
     }
   });
 });
@@ -148,8 +164,9 @@ gulp.task('browser-sync', () => {
 gulp.task('styles', gulp.series('scss', 'scsslint'));
 
 gulp.task('watch', () => {
-  gulp.watch(paths.styles, gulp.series('styles')).on('change', reload);
-  gulp.watch(paths.scripts, gulp.series('scripts', 'eslint')).on('change', reload);
+  gulp.watch(paths.styles.src, gulp.series('styles')).on('change', reload);
+  gulp.watch(paths.scripts.src, gulp.series('scripts', 'eslint')).on('change', reload);
+  gulp.watch(paths.images.src, gulp.series('optimize-images')).on('change', reload);
 });
 
 gulp.task('icons', gulp.series('optimize-images', 'iconfont', 'styles'));
